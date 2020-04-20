6437d65ac6103d2c2be349f8df80538092371054
Bojan <dbojan@gmail.com>
2020-04-20 17:53:03 -0300

    fix wrapped generated code and go mod files

--- internal/wrapped/wrapped.pb.go
+++ internal/wrapped/wrapped.pb.go
@@ -25,9 +25,7 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
-func init() {
-	proto.RegisterFile("wrapped.proto", fileDescriptor_a9aa7f80a2b4490d)
-}
+func init() { proto.RegisterFile("wrapped.proto", fileDescriptor_a9aa7f80a2b4490d) }
 
 var fileDescriptor_a9aa7f80a2b4490d = []byte{
 	// 135 bytes of a gzipped FileDescriptorProto
@@ -44,11 +42,11 @@ var fileDescriptor_a9aa7f80a2b4490d = []byte{
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
-var _ grpc.ClientConnInterface
+var _ grpc.ClientConn
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion6
+const _ = grpc.SupportPackageIsVersion4
 
 // WrappedServiceClient is the client API for WrappedService service.
 //
@@ -58,10 +56,10 @@ type WrappedServiceClient interface {
 }
 
 type wrappedServiceClient struct {
-	cc grpc.ClientConnInterface
+	cc *grpc.ClientConn
 }
 
-func NewWrappedServiceClient(cc grpc.ClientConnInterface) WrappedServiceClient {
+func NewWrappedServiceClient(cc *grpc.ClientConn) WrappedServiceClient {
 	return &wrappedServiceClient{cc}
 }
 
