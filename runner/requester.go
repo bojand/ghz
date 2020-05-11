bb357e219d18facedb98789de905db8af9396e53
Bojan <dbojan@gmail.com>
2020-05-10 22:04:28 -0300

    enhance: increase default client max message size

--- runner/requester.go
+++ runner/requester.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 	"sync"
@@ -312,6 +313,13 @@ func (b *Requester) newClientConn(withStatsHandler bool) (*grpc.ClientConn, erro
 	if b.config.hasLog {
 		b.config.log.Debugw("Creating client connection", "options", opts)
 	}
+
+	// increase max receive and send message sizes
+	opts = append(opts,
+		grpc.WithDefaultCallOptions(
+			grpc.MaxCallRecvMsgSize(math.MaxInt32),
+			grpc.MaxCallSendMsgSize(math.MaxInt32),
+		))
 
 	// create client connection
 	return grpc.DialContext(ctx, b.config.host, opts...)
